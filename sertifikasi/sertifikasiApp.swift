b0989afd25dde6df7f6bcd579230f557767ecdd3
GTerrence <tpramono@student.ciputra.ac.id>
2022-12-18 08:24:11 +0700

    Initial Commit

---
+++ sertifikasi/sertifikasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sertifikasiApp.swift
+//  sertifikasi
+//
+//  Created by Terrence Pramono on 18/12/22.
+//
+
+import SwiftUI
+
+@main
+struct sertifikasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
